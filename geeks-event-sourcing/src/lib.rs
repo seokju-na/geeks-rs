617745697a2712fe34b3e4f137e879b2d087bc8f
Seokju Na <seokju.me@toss.im>
2022-05-15 17:00:56 +0900

    snapshot

--- geeks-event-sourcing/src/lib.rs
+++ geeks-event-sourcing/src/lib.rs
@@ -4,12 +4,76 @@ pub use crate::aggregate::{Aggregate, AggregateRoot};
 pub use crate::command::Command;
 pub use crate::event::{Event, PersistedEvent};
 pub use crate::eventstore::*;
+pub use crate::snapshot::*;
 
 mod aggregate;
 mod command;
 mod event;
 mod eventstore;
+mod snapshot;
 pub mod testing;
 
 pub type Version = u64;
 pub type Timestamp = i64;
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error<E, EE, SE> {
+  #[error("aggregate error: {0}")]
+  AggregateError(#[source] E),
+
+  #[error("eventstore error: {0}")]
+  EventstoreError(#[source] EE),
+
+  #[error("snapshot error: {0}")]
+  SnapshotError(#[source] SE),
+}
+
+pub async fn get_unsaved_events<T, E>(
+  root: &AggregateRoot<T>,
+  eventstore: &E,
+) -> Result<Vec<PersistedEvent<T::Event>>, E::Error>
+where
+  T: Aggregate,
+  E: Eventstore<Event = T::Event>,
+{
+  let versions = root.versions.clone();
+  let mut unsaved_events = Vec::new();
+  let read_events = versions.iter().map(|(id, version)| async {
+    eventstore
+      .read(id.to_owned(), VersionSelect::From(*version + 1))
+      .await
+  });
+
+  for events in read_events {
+    let events = events.await?;
+    unsaved_events.append(&mut events.clone());
+  }
+
+  Ok(unsaved_events)
+}
+
+pub async fn load_aggregate<T, E, S>(
+  eventstore: &E,
+  snapshot: &S,
+) -> Result<AggregateRoot<T>, Error<T::Error, E::Error, S::Error>>
+where
+  T: Aggregate,
+  E: Eventstore<Event = T::Event>,
+  S: Snapshot<T>,
+{
+  let mut root = snapshot.load().await.map_err(Error::SnapshotError)?;
+  let unsaved_events = get_unsaved_events(&root, eventstore)
+    .await
+    .map_err(Error::EventstoreError)?;
+
+  root
+    .save_events(unsaved_events)
+    .map_err(Error::AggregateError)?;
+
+  snapshot
+    .save(root.clone())
+    .await
+    .map_err(Error::SnapshotError)?;
+
+  Ok(root)
+}
